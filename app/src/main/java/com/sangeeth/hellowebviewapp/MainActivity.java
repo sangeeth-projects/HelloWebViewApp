5293799931838e8bdc507cc5d86c31e88a84ae7c
sangeeth.projects <sangeeth.projects@gmail.com>
2020-06-23 01:22:29 +0530

    Initial commit

---
+++ app/src/main/java/com/sangeeth/hellowebviewapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sangeeth.hellowebviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView =(WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
